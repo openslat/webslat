201c2d5937fe21fe070f8d8489fe91d75a32540d
Michael Gauland <michael.gauland@canterbury.ac.nz>
2017-10-06 15:47:21 +1300

    Descriptions working.

--- webslat/slat/views.py
+++ webslat/slat/views.py
@@ -1099,6 +1099,11 @@ class ComponentAutocomplete(autocomplete.Select2QuerySetView):
 
 def ComponentDescription(request, component_key):
     print("> ComponentDescription(..., ", component_key, ")")
+    component = ComponentsTab.objects.get(pk=component_key)
+    print(component)
     result = render(request, 'slat/component-description.html', 
-                    {'component': ComponentsTab.objects.get(pk=component_key)})
+                    {'component': component,
+                     'fragility': FragilityTab.objects.filter(component = component).order_by('state'),
+                     'costs': CostTab.objects.filter(component = component).order_by('state')})
+    print(result)
     return(result)
