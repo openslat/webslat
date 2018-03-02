df3ba65a385ebc4a0858c5ea9f9cc2a65e0c0b1e
Michael Gauland <michael.gauland@canterbury.ac.nz>
2018-03-02 14:22:39 +1300

    Add password reset page.

--- webslat/slat/urls.py
+++ webslat/slat/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 
 from . import views
 from . import component_views
@@ -9,6 +10,11 @@ urlpatterns = [
         views.SLATRegistrationView.as_view(),
         name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'^$', views.index, name='index'),
     url(r'^project/(?P<project_id>[0-9]+)$', views.project, name='project'),
     url(r'^project$', views.project, name='project'),
--- webslat/webslat/settings.py
+++ webslat/webslat/settings.py
@@ -132,3 +132,10 @@ LOGIN_URL='/login'
 LOGIN_REDIRECT_URL= '/slat'
 LOGOUT_REDIRECT_URL='/login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'openslat@gmail.com'
+EMAIL_HOST_PASSWORD = 'OnIl?OgHibEnmyabcub9'
+
