984b713e0d0d2d2f6532548e368901756b8fd1eb
Michael Gauland <michael.gauland@canterbury.ac.nz>
2018-08-16 11:03:23 +1200

    Make start-celery script more flexible.

--- start-celery.sh
+++ start-celery.sh
@@ -1,6 +1,8 @@
 #! /bin/sh -l
-. /home/webslat-user/.profile
-cd /home/webslat-user
-. ./webslat-env/bin/activate
-cd webslat/webslat
+basedir=$(dirname $0)
+homedir=$(echo $0 | sed -e 's_\(/home/[^/]*\).*_\1_')
+. $homedir/webslat-env/bin/activate
+export LD_LIBRARY_PATH=$homedir/SLAT/linux/lib
+export PYTHONPATH=$homedir/SLAT/linux/lib
+cd $basedir/webslat
 celery -A celery_tasks worker
